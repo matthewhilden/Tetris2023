af581d6a6f7724c2417cf0e22d8e756427162363
matthewhilden <matt.hilden@gmail.com>
2023-04-16 16:35:42 -0700

    Added OnPaint Function
    GameBoardFrame::OnPaint

--- include/GameBoardFrame.h
+++ include/GameBoardFrame.h
@@ -10,20 +10,27 @@
 //const int KEY_CODE_ROTATE_CW = ??
 //const int KEY_CODE_ROTATE_CCW = ??
 
-const int FRAME_X_OFFSET = 500;
-const int FRAME_Y_OFFSET = 25;
+const bool GAME_BOARD_SPAWN = false;
 
-const int FRAME_WIDTH = 1000;
-const int FRAME_HEIGHT = 1000;
+const wxColour ORANGE = wxColor(255, 165, 0);
+const wxColour PURPLE = wxColour(128, 0, 128);
 
-const int PANEL_BOARD_HEIGHT = 800;
-const int PANEL_BOARD_WIDTH = PANEL_BOARD_HEIGHT / 2;
+const int WINDOW_X_OFFSET = 500;
+const int WINDOW_Y_OFFSET = 25;
 
-const int PANEL_BOARD_CELL_HEIGHT = PANEL_BOARD_HEIGHT / BOARD_HEIGHT;
-const int PANEL_BOARD_CELL_WIDTH = PANEL_BOARD_WIDTH / BOARD_WIDTH;
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 1000;
 
-const int PANEL_BOARD_X_OFFSET = (FRAME_WIDTH / 2) - (PANEL_BOARD_WIDTH);
-const int PANEL_BOARD_Y_OFFSET = (FRAME_HEIGHT / 2) - (PANEL_BOARD_HEIGHT);
+const double WINDOW_HEIGHT_RATIO = 0.8;
+
+const int FRAME_BOARD_HEIGHT = WINDOW_HEIGHT_RATIO * WINDOW_HEIGHT;
+const int FRAME_BOARD_WIDTH = FRAME_BOARD_HEIGHT / 2;
+
+const int FRAME_BOARD_X_OFFSET = (WINDOW_WIDTH / 2) - (FRAME_BOARD_WIDTH / 2);
+const int FRAME_BOARD_Y_OFFSET = (WINDOW_HEIGHT / 2) - (FRAME_BOARD_HEIGHT / 2);
+
+const int FRAME_BOARD_CELL_HEIGHT = FRAME_BOARD_HEIGHT / BOARD_HEIGHT;
+const int FRAME_BOARD_CELL_WIDTH = FRAME_BOARD_WIDTH / BOARD_WIDTH;
 
 class GameBoardFrame : public wxFrame
 {
@@ -35,6 +42,7 @@ class GameBoardFrame : public wxFrame
 
     private:
 
+        bool visible_spawn;
         Board game_board;
 
         void OnPaint(wxPaintEvent & event);
--- src/GameBoardFrame.cpp
+++ src/GameBoardFrame.cpp
@@ -2,13 +2,125 @@
 
 #include "GameBoardFrame.h"
 
-GameBoardFrame::GameBoardFrame() : wxFrame(nullptr, wxID_ANY, "Tetris")
+GameBoardFrame::GameBoardFrame() : wxFrame(nullptr, wxID_ANY, "Tetris"), visible_spawn(GAME_BOARD_SPAWN)
 {
-    SetSize(FRAME_X_OFFSET, FRAME_Y_OFFSET, FRAME_WIDTH, FRAME_HEIGHT, wxSIZE_AUTO);
+    // ** NEED TO HANDLE WINDOW RESIZING **
+
+    SetSize(WINDOW_X_OFFSET, WINDOW_Y_OFFSET, WINDOW_WIDTH, WINDOW_HEIGHT, wxSIZE_AUTO);
     Show(true);
 }
 
 void GameBoardFrame::OnPaint(wxPaintEvent & event)
 {
-    
+    wxPaintDC dc(this);
+
+    dc.SetPen(*wxTRANSPARENT_PEN);
+    dc.SetBrush(*wxWHITE_BRUSH);
+    dc.DrawRectangle(wxRect(FRAME_BOARD_X_OFFSET, FRAME_BOARD_Y_OFFSET, FRAME_BOARD_WIDTH, FRAME_BOARD_HEIGHT));
+
+    // Draw cells
+    for (int column = 0; column < BOARD_WIDTH; column++)
+    {
+        for (int row = BOARD_HEIGHT - 1; row >= 0; row--)
+        {
+            int x_coord = FRAME_BOARD_X_OFFSET + column * FRAME_BOARD_CELL_WIDTH;
+            int y_coord = FRAME_BOARD_Y_OFFSET + row * FRAME_BOARD_CELL_HEIGHT;
+            int cell_value = game_board.get_cell(column, row);
+            switch (cell_value)
+            {
+                case 1  :   dc.SetBrush(*wxCYAN_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 2  :   dc.SetBrush(*wxBLUE_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 3  :   dc.SetBrush(wxBrush(ORANGE, wxBRUSHSTYLE_SOLID));
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 4  :   dc.SetBrush(*wxYELLOW_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 5  :   dc.SetBrush(*wxGREEN_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 6  :   dc.SetBrush(wxBrush(PURPLE, wxBRUSHSTYLE_SOLID));
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                case 7  :   dc.SetBrush(*wxRED_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                            break;
+                default :   dc.SetBrush(*wxTRANSPARENT_BRUSH);
+                            dc.SetPen(*wxGREY_PEN);
+                            dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+            }
+        }
+    }
+    dc.SetPen(*wxBLACK_PEN);
+    dc.SetBrush(*wxTRANSPARENT_BRUSH);
+    dc.DrawRectangle(wxRect(FRAME_BOARD_X_OFFSET, FRAME_BOARD_Y_OFFSET, FRAME_BOARD_WIDTH, FRAME_BOARD_HEIGHT));
+
+    if (visible_spawn)
+    {
+        int spawn_y_height = BOARD_HEIGHT_BUFFER * FRAME_BOARD_CELL_HEIGHT;
+        int spawn_y_offset = FRAME_BOARD_Y_OFFSET - spawn_y_height;
+
+        dc.SetPen(*wxTRANSPARENT_PEN);
+        dc.SetBrush(*wxWHITE_BRUSH);
+        dc.DrawRectangle(wxRect(FRAME_BOARD_X_OFFSET, spawn_y_offset, FRAME_BOARD_WIDTH, spawn_y_height));
+
+        // Draw cells
+        for (int column = 0; column < BOARD_WIDTH; column++)
+        {
+            for (int row = BOARD_HEIGHT_BUFFER; row > 0; row--)
+            {
+                int x_coord = FRAME_BOARD_X_OFFSET + (column * FRAME_BOARD_CELL_WIDTH);
+                int y_coord = FRAME_BOARD_Y_OFFSET - (row * FRAME_BOARD_CELL_HEIGHT);
+                int cell_value = game_board.get_cell(column, row + BOARD_HEIGHT - 1);
+                switch (cell_value)
+                {
+                    case 1  :   dc.SetBrush(*wxCYAN_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 2  :   dc.SetBrush(*wxBLUE_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 3  :   dc.SetBrush(wxBrush(ORANGE, wxBRUSHSTYLE_SOLID));
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 4  :   dc.SetBrush(*wxYELLOW_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 5  :   dc.SetBrush(*wxGREEN_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 6  :   dc.SetBrush(wxBrush(PURPLE, wxBRUSHSTYLE_SOLID));
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    case 7  :   dc.SetBrush(*wxRED_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                                break;
+                    default :   dc.SetBrush(*wxTRANSPARENT_BRUSH);
+                                dc.SetPen(*wxGREY_PEN);
+                                dc.DrawRectangle(wxRect(x_coord, y_coord, FRAME_BOARD_CELL_WIDTH, FRAME_BOARD_CELL_HEIGHT));
+                }
+            }
+        }
+        dc.SetPen(*wxBLACK_PEN);
+        dc.SetBrush(*wxTRANSPARENT_BRUSH);
+        dc.DrawRectangle(wxRect(FRAME_BOARD_X_OFFSET, spawn_y_offset, FRAME_BOARD_WIDTH, spawn_y_height));  
+    }
 }
